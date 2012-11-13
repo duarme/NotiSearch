9341af25730edc1dfb3638a9e35435d6baf9b3d2
duccioarmenise <duccioarmenise@gmail.com>
2012-11-13 11:59:36 +0100

    initial commit, .gitignore amended

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notisearch::Application.config.secret_token = '1a24ca0550d174fc66e6c98793ee232a4411d899023711d612a30f3d4be2225dc8d3dce4c99f10ae2610923c32352830192dea140affe3d6e15c4d86469d4e23'
